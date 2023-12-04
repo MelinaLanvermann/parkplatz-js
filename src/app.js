ea48034d6919e7e80e57bd9d015272d4919cf8aa
Melina Lanvermann <melina.lanvermann@adesso.de>
2023-12-04 14:20:44 +0100

    issue: #2

--- src/app.js
+++ src/app.js
@@ -1,2 +1,58 @@
 import {parkingMocks} from "./mock_data.js";
-import {Booking} from "./booking.js";
+// import {Booking} from "./booking.js";
+
+document.body.onload = createParkingList;
+const currentDiv = document.getElementById("parking");
+const newDiv = document.createElement("div");
+newDiv.setAttribute("class", "parking-list")
+
+function createParkingList() {
+
+    for (const spot of parkingMocks) {
+        const newA = document.createElement("a");
+        const newIdDiv = document.createElement("span");
+        const newStatusDiv = document.createElement("span");
+        const newTypeDiv = document.createElement("span");
+        const newLocDiv = document.createElement("span");
+
+        const newIdText = document.createTextNode(`Parkplatz Nr. ${spot.id}`);
+        newIdDiv.appendChild(newIdText);
+        newA.appendChild(newIdDiv);
+
+        if (spot.status === "free") {
+            const newStatusText = document.createTextNode(`Status : frei`);
+            newStatusDiv.appendChild(newStatusText);
+            newA.setAttribute("class", "free");
+        } else if (spot.status === "booked") {
+            const newStatusText = document.createTextNode(`Status : gebucht`);
+            newStatusDiv.appendChild(newStatusText);
+            newA.setAttribute("class", "booked");
+        } else {
+            const newStatusText = document.createTextNode(`Status : nicht abrufbar`);
+            newStatusDiv.appendChild(newStatusText);
+            newA.setAttribute("class", "unknown")
+        }
+        newA.appendChild(newStatusDiv);
+
+        if (spot.type === "wide") {
+            const newTypeText = document.createTextNode(`Dies ist ein breiterer Parkplatz`);
+            newTypeDiv.appendChild(newTypeText);
+        } else {
+            const newTypeText = document.createTextNode(`Dies ist ein normaler Parkplatz`);
+            newTypeDiv.appendChild(newTypeText);
+        }
+        newA.appendChild(newTypeDiv);
+
+        if (spot.distance === "close") {
+            const newLocText = document.createTextNode(`Nah an Ein-/ Ausgang : ja `);
+            newLocDiv.appendChild(newLocText);
+        } else {
+            const newLocText = document.createTextNode(`Nah an Ein-/ Ausgang : nein `);
+            newLocDiv.appendChild(newLocText);
+        }
+        newA.appendChild(newLocDiv);
+        newA.setAttribute("id", `${spot.id}`);
+        newDiv.appendChild(newA);
+    }
+    document.body.replaceChild(newDiv, currentDiv);
+}
